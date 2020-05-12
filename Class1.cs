aefeff1b082fdfb5d99a1bf9e2414b7b466e6a9c
Денис El <elserov@yandex.ru>
2020-05-12 13:02:46 +0300

    mindbox

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mindbox
+{
+    public class Class1
+    {
+        double aria (double ariaСircle)// Вычисление площади круга
+        {
+            return Math.PI * Math.Pow(ariaСircle, 2); 
+        }
+        double aria (double a, double b, double c)// Вычесление площади треугольника по трем сторонам
+        {
+            double P = (a + b + c) / 2;
+
+            return Math.Sqrt(P*(P - a)*(P - b)*(P - c)); 
+        }
+    }
+}
